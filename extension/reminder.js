8ae57a15fa0812345d69de6e4ae57a6a2f1731ac
sanz17 <kundusanhita13@gmail.com>
2022-06-28 16:50:34 +0530

    add reminder

--- extension/reminder.js
+++ extension/reminder.js
@@ -1,31 +1,36 @@
-const notify = (heading, content) => {
-    const options = {
-      type: "basic",
-      title: heading,
-      message: content,
-      iconUrl: "icons/icon_128.png",
-    };
-    chrome.notifications.create(options, (id) => {
-      console.log(`notification sent - ${id}`);
+window.onload = function() {
+  document.querySelector('button').addEventListener('click', function() {
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+      console.log(token);
     });
-  };
+    
+  });
+};
 
-document.querySelector("input[type='submit']").addEventListener("click", (e) => {
-    console.log("from reminder.js");
-    const email = document.querySelector("input[name='email']").value;
-    try {
-        chrome.storage.sync.set({ email }, () => {
-          alert("Email saved locally !!");
-          notify("Your'e all set", "Email saved locally");
-          chrome.storage.sync.set({ login_count: 0 }, (res) => {
-            console.log(`login count - ${res.login_count}`);
-          });
-        });
-      } catch (error) {
-        alert("Unable to save email :(");
-        notify(
-          "Something went wrong :(",
-          "Unable to save email locally !!"
-        );
-      }
+const notify = (heading, content) => {
+  const options = {
+    type: "basic",
+    title: heading,
+    message: content,
+    iconUrl: "icons/icon_128.png",
+  };
+  chrome.notifications.create(options, (id) => {
+    console.log('notification sent - ${id}');
+  });
+};
+document.querySelector('.submit-btn').addEventListener("click", function() {
+  console.log("click event listener is working | from reminder.js");
+  const email = document.querySelector("input[name='email']").value;
+  try {
+    chrome.storage.sync.set({ email }, () => {
+      alert("email saved locally !!");
+      notify("Your'e all set", "Username and Password saved locally");
+    });
+  } catch (error) {
+    alert("Unable to save Username and Password locally :(");
+    notify(
+      "Something went wrong :(",
+      "Unable to save Username and Password locally !!"
+    );
+  }
 });
