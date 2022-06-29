2c5cc628de692058f405bc4e07d7deabc44b8968
sanz17 <kundusanhita13@gmail.com>
2022-06-29 20:28:52 +0530

    add reminder

--- extension/reminder.js
+++ extension/reminder.js
@@ -1,11 +1,11 @@
-window.onload = function() {
-  document.querySelector('button').addEventListener('click', function() {
-    chrome.identity.getAuthToken({interactive: true}, function(token) {
-      console.log(token);
-    });
+// window.onload = function() {
+//   document.querySelector('button').addEventListener('click', function() {
+//     chrome.identity.getAuthToken({interactive: true}, function(token) {
+//       console.log(token);
+//     });
     
-  });
-};
+//   });
+// };
 
 const notify = (heading, content) => {
   const options = {
